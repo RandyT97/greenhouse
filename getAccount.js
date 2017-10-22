12b33ab7c200c79051b9129566ed86f721c5a47d
warren <warrenoliver1215@gmail.com>
2017-10-22 11:34:47 -0500

    test

--- getAccount.js
+++ getAccount.js
@@ -12,7 +12,7 @@ var count = +1;
 function putChart(accs) {
   var accts = accs;
   for(var i = 0; i < accts.length; i++) {
-    var tr = '<tr><td>' + count + '</td><td> ' + accts[i].type + '</td><td> ' + accts[i].rewards +
+    var tr = '<tr><td>' + count + '</td><td> ' + accts[i].type + '</td><td> ' + "$" + accts[i].rewards +
     '</td><td> ' + "$" + accts[i].balance + '</td></tr>';
     $('#table').append(tr);
     count++;

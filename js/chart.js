c263f7e2682f2e84b4ab0e7c0f7b3f4b4dab88ed
warren <warrenoliver1215@gmail.com>
2017-10-22 18:29:22 -0500

    organized files

---
+++ js/chart.js
@@ -0,0 +1,84 @@
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ["Jan", "Feb", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    datasets: [{
+      label: "Sessions",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 20,
+      pointBorderWidth: 2,
+      data: [6900, 4200, 6900, 2000, 42000, 34343, 65600, 85755, 25232, 15400, 45000, 43022],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+var counter = +0;
+for(var i = 0; i <= 11; i++)
+{
+  console.log(myLineChart.data.labels[counter]);
+  myLineChart.data.labels[counter] = 5000;
+  counter++;
+}
+myLineChart.render();
+
+//Pie Chart
+var ctxP = document.getElementById("pieChart").getContext('2d');
+var myPieChart = new Chart(ctxP,
+  {
+    type: 'pie',
+    data:
+    {
+        labels: ["Bills", "Recreation", "Food", "Gas", "Excess"],
+        datasets:
+        [
+            {
+                data: [50, 15, 15, 17, 3],
+                backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+                hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+            }
+        ]
+    },
+    options:
+    {
+        responsive: true
+    }
+});

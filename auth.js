b494b1d381fafa16aec62954996faf6fc3c46b03
warren <warrenoliver1215@gmail.com>
2017-10-23 00:08:55 -0500

    fixed files

--- auth.js
+++
@@ -1,9 +0,0 @@
-const queryParam = "api.reimaginebanking.com/customers?key=a67096375bcf4a60ab80d781a3fabcbc";
-
-$.ajax({
-    url: queryParam,
-    success: function(results){
-        //do something 
-        console.log(results);
-    }
-});
